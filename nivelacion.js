45ce25bde165b5170c0fb3741cb5a0e822b52a57
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-02-11 09:55:07 -0500

    ej1

---
+++ nivelacion.js
@@ -0,0 +1,11 @@
+function obtenerPar(numero){
+if ( numero%2===0){
+    return "es numero par";
+    }
+else{
+    return "el numero es impar";
+}
+}
+
+let resultado = obtenerPar(50)
+console.log(resultado)
